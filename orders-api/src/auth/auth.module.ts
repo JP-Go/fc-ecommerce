7dddb9a6663339f9a0445fc3fec26cee8537f941
João Pedro <jpedro1580@gmail.com>
2024-05-31 19:57:21 -0300

    feat: export auth guard globally

--- orders-api/src/auth/auth.module.ts
+++ orders-api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
 
+@Global()
 @Module({
   imports: [
     JwtModule.register({
@@ -14,6 +16,7 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AuthModule {}
